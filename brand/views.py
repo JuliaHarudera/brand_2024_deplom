13069c9647355c0eaedd6471a118b3678eb7ff8d
juliaharudera <juliaharudera1@gmail.com>
2024-01-03 22:20:54 +0200

    не знание с чего начать в html

--- brand/views.py
+++ brand/views.py
@@ -6,3 +6,11 @@ def main(request):
     category = Category.objects.filter(is_visible=True)
     return render(request, 'index.html')
 
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+

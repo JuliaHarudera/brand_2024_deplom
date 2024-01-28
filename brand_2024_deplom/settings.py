165ed511d679e0cc78e0fc5f656703ab7931704a
juliaharudera <juliaharudera1@gmail.com>
2024-01-28 10:11:42 +0200

    3

--- brand_2024_deplom/settings.py
+++ brand_2024_deplom/settings.py
@@ -58,8 +58,8 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
